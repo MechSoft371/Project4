d0801aa71cab3e408bf6b6429ea8016471ad07b3
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2024-06-02 14:00:09 +0530

    add configaration for default and page load time out

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     "baseUrl" : 'https://opensource-demo.orangehrmlive.com/',
+    'defaultCommandTimeout' : 100000,
+    'pageLoadTimeout' : 120000,
+    retries : {openMode: 1},
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
