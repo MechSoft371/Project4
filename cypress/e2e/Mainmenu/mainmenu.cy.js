6957538bbc8f009eabbd04820d830ea7ae57b47c
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2024-06-05 09:51:22 +0530

    configure the  mocha report and add new change on main menu

--- cypress/e2e/Mainmenu/mainmenu.cy.js
+++ cypress/e2e/Mainmenu/mainmenu.cy.js
@@ -17,6 +17,8 @@ describe("check the main menu iteam ", ()=> {
 
         }
 
+        cy.writeFile('cypress/fixtures/headerText.txt', '');
+
         cy.visit('web/index.php/auth/login')
     cy.get('input[name ="username"]').type('Admin')
     cy.get('input[name="password"]').type('admin123')
@@ -31,8 +33,8 @@ describe("check the main menu iteam ", ()=> {
             cy.log(headerText);
 
             // Optionally, you can write this text to a file or use it in assertions
-            cy.writeFile('headerText.txt', headerText.);
-        cy.screenshot(menu[i])
+            cy.writeFile('cypress/fixtures/headerText.txt', headerText + '\n',{ flag: 'a+'});
+        //cy.screenshot(menu[i])
         })
     }
 
@@ -42,4 +44,37 @@ describe("check the main menu iteam ", ()=> {
 
 
     })
+
+    it("Verfiy the each menu iteam by click", ()=>{
+
+        cy.get('.oxd-main-menu > li')
+
+        cy.writeFile('cypress/fixtures/headerText.txt', '');
+
+        cy.visit('web/index.php/auth/login')
+    cy.get('input[name ="username"]').type('Admin')
+    cy.get('input[name="password"]').type('admin123')
+    cy.get('button[type="submit"]').click()
+    cy.contains("Dashboard").should('be.visible')
+
+    for (let i in menu){
+
+        cy.contains(menu[i]).should('be.visible').click()
+        cy.get('.oxd-topbar-header-title').invoke('text').then((headerText) => {
+            // Print the header text to the console
+            cy.log(headerText);
+
+            // Optionally, you can write this text to a file or use it in assertions
+            cy.writeFile('cypress/fixtures/headerText.txt', headerText + '\n',{ flag: 'a+'});
+        //cy.screenshot(menu[i])
+        })
+    }
+
+
+
+
+
+
+    })
+
 })
